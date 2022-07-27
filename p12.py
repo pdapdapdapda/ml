3b2a33b4ab37786ac853a7296bcb5c21b54bd2e1
pdapdapdapda <mohammedmusaibali@gmail.com>
2022-07-27 14:37:54 +0530

    cutting programs

--- p12.py
+++ p12.py
@@ -1,39 +1,13 @@
+import numpy as np
 import math
 import matplotlib.pyplot as plt
-from math import ceil
-import numpy as np
-from scipy import linalg
-
-
-def lowess(x, y, f, iterations):
-    n = len(x)
-    r = int(ceil(f * n))
-    h = [np.sort(np.abs(x - x[i]))[r] for i in range(n)]
-    w = np.clip(np.abs((x[:, None] - x[None, :]) / h), 0.0, 1.0)
-    w = (1 - w ** 3) ** 3
-    yest = np.zeros(n)
-    delta = np.ones(n)
-    for iteration in range(iterations):
-        for i in range(n):
-            weights = delta * w[:, i]
-            b = np.array([np.sum(weights * y), np.sum(weights * y * x)])
-            A = np.array([[np.sum(weights), np.sum(weights * x)],
-                         [np.sum(weights * x), np.sum(weights * x * x)]])
-            beta = linalg.solve(A, b)
-            yest[i] = beta[0] + beta[1] * x[i]
-        residuals = y - yest
-        s = np.median(np.abs(residuals))
-        delta = np.clip(residuals / (6.0 * s), -1, 1)
-        delta = (1 - delta ** 2) ** 2
-    return yest
-
-
+from sklearn.linear_model import LinearRegression
 n = 100
-x = np.linspace(0, 2 * math.pi, n)
-y = np.sin(x) + 0.3 * np.random.randn(n)
-f = 0.25
-iterations = 3
-yest = lowess(x, y, f, iterations)
-plt.plot(x, y, "r.")
-plt.plot(x, yest, "b-")
+xpoints = np.linspace(0, 2*math.pi, n).reshape(-1, 1)
+ypoints = np.sin(xpoints)
+linreg = LinearRegression()
+linreg.fit(xpoints, ypoints)
+prediction = linreg.predict(xpoints)
+plt.scatter(xpoints, ypoints, color='red')
+plt.plot(xpoints, prediction)
 plt.show()
--- p7.py
+++ p7.py
@@ -1,27 +1,15 @@
-from sklearn.metrics import accuracy_score
+from sklearn import datasets
 from sklearn.model_selection import train_test_split
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import GaussianNB
-
-data = pd.read_csv('tennisdata.csv')
-
-x = data.iloc[::, :-1]
-# print("\nThe First 5 values of train data is\n", x.head())
-y = data.iloc[::, -1]
-# print("\nThe first 5 values of Train output is\n", y.head())
-
-le = LabelEncoder()
-x.Outlook = le.fit_transform(x.Outlook)
-x.Temperature = le.fit_transform(x.Temperature)
-x.Humidity = le.fit_transform(x.Humidity)
-x.Windy = le.fit_transform(x.Windy)
-# print("\nNow the Train data is :\n", x.head())
-y = le.fit_transform(y)
-# print("\nNow the Train output is\n", y)
-
-X_train, X_test, y_train, y_test = train_test_split(x, y)
-classifier = GaussianNB()
-classifier.fit(X_train, y_train)
-
-print("Accuracy is:", accuracy_score(y_test, classifier.predict(X_test)))
+from sklearn import metrics
+iris = datasets.load_iris()
+print("Features: ", iris.feature_names)
+print("Labels: ", iris.target_names)
+print(iris.data[0:5])
+print(iris.target)
+X_train, X_test, y_train, y_test = train_test_split(
+    iris.data, iris.target)
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+y_pred = gnb.predict(X_test)
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
